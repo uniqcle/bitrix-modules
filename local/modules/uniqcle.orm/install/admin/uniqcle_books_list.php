6fcd45cc63b2f0ae8f4a1cd4a5ccc6cb48d3fb66
Andrey <uniqcle@yandex.ru>
2024-04-02 12:44:22 +0700

    admin list

--- local/modules/uniqcle.orm/install/admin/uniqcle_books_list.php
+++ local/modules/uniqcle.orm/install/admin/uniqcle_books_list.php
@@ -3,11 +3,11 @@
 // определяем в какой папке находится модуль, если в bitrix, инклудим файл с меню из папки bitrix
 if (is_dir($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/uniqcle.orm/")) {
 	// присоединяем и копируем файл
-	require_once($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/uniqcle.orm/admin/uniqcle_admin.php");
+	require_once($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/uniqcle.orm/admin/uniqcle_books_list.php");
 }
 
 // определяем в какой папке находится модуль, если в local, инклудим файл с меню из папки local
 if (is_dir($_SERVER["DOCUMENT_ROOT"] . "/local/modules/uniqcle.orm/")) {
 	// присоединяем и копируем файл
-	require_once($_SERVER["DOCUMENT_ROOT"] . "/local/modules/uniqcle.orm/admin/uniqcle_admin.php");
+	require_once($_SERVER["DOCUMENT_ROOT"] . "/local/modules/uniqcle.orm/admin/uniqcle_books_list.php");
 }
